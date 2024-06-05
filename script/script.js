37f02eb63c7bbbfb6fa7aa189b026e3d53e4785a
alexanderkalyupanov <138789735+alexanderkalyupanov@users.noreply.github.com>
2024-06-05 14:18:43 +0300

    add changed portfolio

--- script/script.js
+++ script/script.js
@@ -12,3 +12,22 @@ let swiper = new Swiper(".newSwiper", {
     prevEl: ".swiper-button-prev",
   },
 });
+
+// Tabs
+
+function openTab(event, tabName) {
+  var i, tabcontent, tablinks;
+  // Скрываем все элементы с классом "tab-content"
+  tabcontent = document.getElementsByClassName("tab-content");
+  for (i = 0; i < tabcontent.length; i++) {
+  tabcontent[i].style.display = "none";
+  }
+  // Удаляем класс "active" со всех кнопок с классом "tablinks"
+  tablinks = document.getElementsByClassName("tab-link");
+  for (i = 0; i < tablinks.length; i++) {
+  tablinks[i].classList.remove("active");
+  }
+  // Отображаем текущую вкладку и добавляем класс "active" к кнопке, которая открывает эту вкладку
+  document.getElementById(tabName).style.display = "block";
+  event.currentTarget.classList.add("active");
+  }
