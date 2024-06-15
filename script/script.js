560f863a8ce78a922fb29d6d8f51a04871823380
alexanderkalyupanov <138789735+alexanderkalyupanov@users.noreply.github.com>
2024-06-15 14:10:17 +0300

    add adaptive in index page

--- script/script.js
+++ script/script.js
@@ -13,6 +13,16 @@ let swiper = new Swiper(".swiper-feedback", {
   },
 });
 
+// Menu
+
+function openModalMenu() {
+  document.getElementById("overlayMenu").style.top = "0px";
+}
+function closeModalMenu() {
+  document.getElementById("overlayMenu").style.top = "-100vh";
+}
+
+
 // Tabs
 
 function openTab(event, tabName) {
